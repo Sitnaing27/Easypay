2d04a28ae9e84232e18e463f686f0639c284bf3a
Sitnaing27 <sai400890@gmail.com>
2026-02-23 21:10:18 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const rate = 125;
-
-function demoTransfer() {
-  const thb = document.getElementById("thb").value;
-  const mmk = thb * rate;
-  document.getElementById("mmk").value = mmk.toLocaleString();
-  alert("Demo transfer successful (No real money involved)");
-}
